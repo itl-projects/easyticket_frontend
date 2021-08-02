c8cf8f3d6df983269ab3632e3ecf7d0706232660
Bhola Kumar <sfs.bhola21@gmail.com>
2021-08-02 23:22:11 +0530

    added server url

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { store } from '../store';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/'
+  baseURL: 'https://easyticketapp.herokuapp.com/api/v1/'
 });
 
 // This allows you to intercept the request before it is sent and alter headers or anyting else that is passed to the axios config.
